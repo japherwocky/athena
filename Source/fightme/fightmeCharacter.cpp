e4fb01cf52c0a364de4fdd017886a85b5be26a81
Japherwocky <japhy@pearachute.com>
2022-02-18 23:32:14 -0500

    clean out kind of flawed tutorial

--- Source/fightme/fightmeCharacter.cpp
+++ Source/fightme/fightmeCharacter.cpp
@@ -1,152 +1,41 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+
 
 #include "fightmeCharacter.h"
-#include "HeadMountedDisplayFunctionLibrary.h"
-#include "Camera/CameraComponent.h"
-#include "Components/CapsuleComponent.h"
-#include "Components/InputComponent.h"
-#include "GameFramework/CharacterMovementComponent.h"
-#include "GameFramework/Controller.h"
-#include "GameFramework/SpringArmComponent.h"
-
-#include "PlayerStatTracker.h"
 
 #include "MyCharacterMovementComponent.h"
 
 //////////////////////////////////////////////////////////////////////////
-// AfightmeCharacter
+// Base Character to set our custom movement component
 
 AfightmeCharacter::AfightmeCharacter(const class FObjectInitializer& ObjectInitializer) :
 	Super(ObjectInitializer.SetDefaultSubobjectClass<UMyCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
 {
-	// Set size for collision capsule
-	// GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);  // we handle this in BPs for now, see ShooterCharacter
 
-	// set our turn rates for input
-	BaseTurnRate = 45.f;
-	BaseLookUpRate = 45.f;
-
-	// Don't rotate when the controller rotates. Let that just affect the camera.
-	// bUseControllerRotationPitch = false;
-	// bUseControllerRotationYaw = false;
-	// bUseControllerRotationRoll = false;
-
-	// Configure character movement
-	// GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	// GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
-	// GetCharacterMovement()->JumpZVelocity = 600.f;
-	// GetCharacterMovement()->AirControl = 0.2f;
-
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
-
-	StatTracker = CreateDefaultSubobject<UPlayerStatTracker>("StatTracker");
-
-	// Use this like 
-	// StatTracker->Hurt(1);
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-//////////////////////////////////////////////////////////////////////////
-// Input
-
-void AfightmeCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+// Called when the game starts or when spawned
+void AfightmeCharacter::BeginPlay()
 {
-	// Set up gameplay key bindings
-	check(PlayerInputComponent);
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	Super::BeginPlay();
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AfightmeCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &AfightmeCharacter::MoveRight);
-
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &AfightmeCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &AfightmeCharacter::LookUpAtRate);
-
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &AfightmeCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &AfightmeCharacter::TouchStopped);
-
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AfightmeCharacter::OnResetVR);
 }
 
-
-void AfightmeCharacter::OnResetVR()
+// Called every frame
+void AfightmeCharacter::Tick(float DeltaTime)
 {
-	// If fightme is added to a project via 'Add Feature' in the Unreal Editor the dependency on HeadMountedDisplay in fightme.Build.cs is not automatically propagated
-	// and a linker error will result.
-	// You will need to either:
-	//		Add "HeadMountedDisplay" to [YourProject].Build.cs PublicDependencyModuleNames in order to build successfully (appropriate if supporting VR).
-	// or:
-	//		Comment or delete the call to ResetOrientationAndPosition below (appropriate if not supporting VR)
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+	Super::Tick(DeltaTime);
+
 }
 
-void AfightmeCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
+// Called to bind functionality to input
+void AfightmeCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-		Jump();
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
 }
-
-void AfightmeCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		StopJumping();
-}
-
-void AfightmeCharacter::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-}
-
-void AfightmeCharacter::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
-}
-
-void AfightmeCharacter::MoveForward(float Value)
-{
-	if ((Controller != nullptr) && (Value != 0.0f))
-	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, Value);
-	}
-}
-
-void AfightmeCharacter::MoveRight(float Value)
-{
-	if ( (Controller != nullptr) && (Value != 0.0f) )
-	{
-		// find out which way is right
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
-		// get right vector 
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
-		AddMovementInput(Direction, Value);
-	}
-}
-
-FString AfightmeCharacter::ReturnPlayerStats()
-{
-	FString* out = new FString("Health: ");
-	out->AppendInt(StatTracker->getHealth());
-
-	return *out;
-}
-
 
 UMyCharacterMovementComponent* AfightmeCharacter::GetMyMovementComponent() const
 {
--- Source/fightme/fightmeCharacter.h
+++ Source/fightme/fightmeCharacter.h
@@ -15,66 +15,18 @@ public:
 	// Sets default values for this character's properties
 	AfightmeCharacter(const class FObjectInitializer& ObjectInitializer);
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 public:
-	AfightmeCharacter();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
-
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
-
-
-	// Our special network friendly movement component
-	UFUNCTION(BlueprintCallable, Category = "Movement")
-	UMyCharacterMovementComponent* GetMyMovementComponent() const;
-
-
-
-protected:
-	class UPlayerStatTracker* StatTracker;
-
-
-	UFUNCTION(BlueprintPure)
-		FString ReturnPlayerStats();
-
-
-protected:
-
-	/** Resets HMD orientation in VR. */
-	void OnResetVR();
-
-	/** Called for forwards/backward input */
-	void MoveForward(float Value);
-
-	/** Called for side to side input */
-	void MoveRight(float Value);
-
-	/** 
-	 * Called via input to turn at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
-
-	/** Handler for when a touch input begins. */
-	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
-
-	/** Handler for when a touch input stops. */
-	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
-
-protected:
-	// APawn interface
+	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	// End of APawn interface
 
+	// Gets the character's MyCustomMovementComponent
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+		UMyCharacterMovementComponent* GetMyMovementComponent() const;
 };
-
